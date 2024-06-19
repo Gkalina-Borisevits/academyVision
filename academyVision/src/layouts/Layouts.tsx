0268ed97c9d84271c98b70135bfd82530600718a
ukoval400 <ukoval400@gmail.com>
2024-06-19 20:52:48 +0200

    add conponent burger-menu

--- academyVision/src/layouts/Layouts.tsx
+++ academyVision/src/layouts/Layouts.tsx
@@ -1,10 +1,12 @@
 import type { FC } from "react";
 import { Outlet } from "react-router-dom";
 import styles from "./Layouts.module.css";
+import Header from "../components/header/Header";
 
 const Layout: FC = () => {
   return (
     <div>
+      <Header/>
       <main className={styles.outletContainer}>
         <Outlet />
       </main>
