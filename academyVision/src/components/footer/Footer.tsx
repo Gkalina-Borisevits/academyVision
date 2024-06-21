09cad358daefcf5b7e9ca61d6951233aa92035de
Gkalina-Borisevits <gkalinaborisevits@gmail.com>
2024-06-21 20:50:28 +0200

    feature: SocialLink in Layout

--- academyVision/src/components/footer/Footer.tsx
+++ academyVision/src/components/footer/Footer.tsx
@@ -3,31 +3,38 @@ import { NavLink } from "react-router-dom";
 import styles from "./Footer.module.css";
 import { FaArrowRight } from "react-icons/fa";
 
-
-
 const Footer: React.FC = () => {
-    const { t } = useTranslation("translation");
+  const { t } = useTranslation("translation");
   return (
     <>
-    <div className={styles.footerContainer}>
-        
+      <div className={styles.footerContainer}>
         <div>
-        <NavLink to="/" className={styles.logoContainer}></NavLink>
+          <NavLink to="/" className={styles.logoContainer}></NavLink>
         </div>
         <div className={styles.linkContainer}>
-        <NavLink to="/about"><FaArrowRight size={18} />{" "}{t("header.about")}</NavLink>
-        <NavLink to="/services"><FaArrowRight size={18} />{" "}{t("header.services")}</NavLink>
-        <NavLink to="/price"><FaArrowRight size={18} />{" "}{t("header.price")}</NavLink>
-        <NavLink to="/portfolio"><FaArrowRight size={18} />{" "}{t("header.portfolio")}</NavLink>
-        <NavLink to="/contacts"><FaArrowRight size={18} />{" "}{t("header.contacts")}</NavLink>
+          <NavLink to="/about">
+            <FaArrowRight size={18} /> {t("header.about")}
+          </NavLink>
+          <NavLink to="/services">
+            <FaArrowRight size={18} /> {t("header.services")}
+          </NavLink>
+          <NavLink to="/price">
+            <FaArrowRight size={18} /> {t("header.price")}
+          </NavLink>
+          <NavLink to="/portfolio">
+            <FaArrowRight size={18} /> {t("header.portfolio")}
+          </NavLink>
+          <NavLink to="/contacts">
+            <FaArrowRight size={18} /> {t("header.contacts")}
+          </NavLink>
         </div>
-      
-    </div>
-    <div className={styles.dateContainer}>
-    <span> © {new Date().getFullYear()}  Academy Mincept Vision </span>
-    </div>
+        
+      </div>
+      <div className={styles.dateContainer}>
+        <span> © {new Date().getFullYear()} Academy Mincept Vision </span>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- academyVision/src/layouts/Layouts.tsx
+++ academyVision/src/layouts/Layouts.tsx
@@ -3,14 +3,19 @@ import { Outlet } from "react-router-dom";
 import styles from "./Layouts.module.css";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import SocialLink from "../components/socialLink/SocialLink";
 
 const Layout: FC = () => {
   return (
     <div>
       <Header/>
+      <div className={styles.socialLinkContainer}>
+          <SocialLink />
+        </div>
       <main className={styles.outletContainer}>
         <Outlet />
       </main>
+     
       <Footer/>
     </div>
   );
