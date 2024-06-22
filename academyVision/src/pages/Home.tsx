52c9a5b7f3aa5c5384ade7ab68310b006d84598e
Gkalina-Borisevits <gkalinaborisevits@gmail.com>
2024-06-22 16:54:32 +0200

    styles: header and home page

--- academyVision/src/pages/Home.tsx
+++ academyVision/src/pages/Home.tsx
@@ -1,34 +1,29 @@
 import { FC } from "react";
 import styles from "./styles/Home.module.css";
 import img from "../assets/home.png";
-import { FC } from 'react'
-import BurgerMenu from '../components/burger-menu/BurgerMenu'
-import HomeCards from '../components/home-cards/HomeCards'
-import { NavLink } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
+import HomeCards from "../components/home-cards/HomeCards";
+import { NavLink } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 const Home: FC = () => {
   return (
-    <div>
-      <div className={styles.imageContainer}>
-        <img src={img} alt="Image" className={styles.backgroundImage} />
-        <div className={styles.overlay}></div>
+    <>
+      <div>
+        <div className={styles.imageContainer}>
+          <img src={img} alt="Image" className={styles.backgroundImage} />
+          <div className={styles.overlay}></div>
+        </div>
+
+        <div className={styles.homeCardContainer}>
+          <Container>
+            <NavLink className="text-decoration-none" to="/services">
+              <HomeCards />
+            </NavLink>
+          </Container>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
-      
-      <Container>
-          <NavLink className="text-decoration-none" to="/services"> 
-         <HomeCards/>
-      </NavLink>
-    
-     
-      </Container>
-    
-      
-      </div>
-  )
-}
 
 export default Home;
