df45fa66e5dbabbdd0ef377c5fdbbdabaac8d936
ukoval400 <ukoval400@gmail.com>
2024-06-30 14:59:39 +0200

    first commit

--- academyVision/src/pages/Services.tsx
+++ academyVision/src/pages/Services.tsx
@@ -2,12 +2,14 @@ import { FC } from "react"
 import styles from "./styles/Services.module.css"
 import img from "../assets/pagesImage/services.png"
 import ImageContainer from "../components/imageContainer/ImageContainer"
+import MoreHomeCards from "../components/home-card-more/MoreHomeCards"
 
 
 const Services: FC = () => {
   return (
     <div className={styles.servicesContainer}>
     <ImageContainer imgSrc={img} imgAlt="Image" />
+    <MoreHomeCards/>
   </div>
   )
 }
