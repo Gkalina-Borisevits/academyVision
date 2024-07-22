8cb13c4fb61d2f4d4d82fda8fac24dadf2da0982
Gkalina-Borisevits <gkalinaborisevits@gmail.com>
2024-07-22 21:20:26 +0200

    fix: formspree

--- academyVision/src/components/contactForm/ContactForm.tsx
+++ academyVision/src/components/contactForm/ContactForm.tsx
@@ -65,7 +65,7 @@ const ContactForm: React.FC = () => {
 
     if (formValid) {
       try {
-        const response = await fetch('https://formspree.io/email_here', {
+        const response = await fetch('https://formspree.io/f/xblrdogo', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
