4e5ec41de2210639796c5af3d5169c58a03447b9
Gkalina-Borisevits <gkalinaborisevits@gmail.com>
2024-07-30 21:40:24 +0200

    fix pages photos

--- academyVision/src/pages/Contacts.tsx
+++ academyVision/src/pages/Contacts.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import styles from "./styles/Contacts.module.css";
-import img from "../assets/pagesImage/contacts.png";
+import img from "../assets/pagesImage/contacts.jpg";
 import ImageContainer from "../components/imageContainer/ImageContainer";
 import ContactUs from "../components/contactUs/ContactUs";
 import MyContainer from "../components/myContainer/MyContainer";
